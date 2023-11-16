a878230cb151f60fcfa52642d607a0e108bbc0d6
Gokhan Bozkurt <gokhan.bozkurt@idb.jsb>
2023-11-16 09:28:55 +0300

    Initial Commit

---
+++ OldConcurrencyLessons/OldConcurrencyLessonsApp.swift
@@ -0,0 +1,17 @@
+//
+//  OldConcurrencyLessonsApp.swift
+//  OldConcurrencyLessons
+//
+//  Created by Gökhan Bozkurt on 16.11.2023.
+//
+
+import SwiftUI
+
+@main
+struct OldConcurrencyLessonsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
